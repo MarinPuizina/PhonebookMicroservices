26e5a0594b56152e336ee73edc67bc341a519b13
MarinPuizina <marinpuizina@hotmail.com>
2019-12-02 22:24:18 +0100

    Add RecordsRepository and ModelMapper

--- Phonebook/src/main/java/com/marin/phonebook/data/RecordsRepository.java
+++ Phonebook/src/main/java/com/marin/phonebook/data/RecordsRepository.java
@@ -3,5 +3,5 @@ package com.marin.phonebook.data;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RecordsRepository extends CrudRepository<RecordEntity, Long> {
-    
+
 }
