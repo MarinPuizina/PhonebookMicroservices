ee95a6d3894dcd64affa6cd9a21953b416393865
MarinPuizina <marinpuizina@hotmail.com>
2019-12-02 16:47:27 +0100

    Add POST createUpdateRecord and CreateUpdateRecordRequestModel

--- Phonebook/src/main/java/com/marin/phonebook/ui/controller/RecordsController.java
+++ Phonebook/src/main/java/com/marin/phonebook/ui/controller/RecordsController.java
@@ -1,10 +1,11 @@
 package com.marin.phonebook.ui.controller;
 
+import com.marin.phonebook.ui.model.request.CreateUpdateRecordRequestModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("records") // http://localhost:8080/records
@@ -16,6 +17,11 @@ public class RecordsController {
     @GetMapping("/status")
     public String getStatus() {
         return "Phonebook Microservice is working on the port." + environment.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String createUpdateRecord(@Valid @RequestBody CreateUpdateRecordRequestModel requestModel) {
+        return "createUpdateRecord() is called.";
     }
 
 }
--- Phonebook/src/main/java/com/marin/phonebook/ui/model/request/CreateUpdateRecordRequestModel.java
+++ Phonebook/src/main/java/com/marin/phonebook/ui/model/request/CreateUpdateRecordRequestModel.java
@@ -2,11 +2,18 @@ package com.marin.phonebook.ui.model.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class CreateUpdateRecordRequestModel {
 
+    @NotNull
     private String personsName;
+
+    @NotNull
     private String recordType;
+
+    @NotNull
     private Integer phoneNumber;
 
 }
