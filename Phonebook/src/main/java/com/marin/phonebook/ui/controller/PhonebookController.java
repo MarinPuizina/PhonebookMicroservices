d442f392cd1b64f5e770a6e58b67fe9c9906c21a
MarinPuizina <marinpuizina@hotmail.com>
2019-12-02 12:47:20 +0100

    Add support for H2 database

--- Phonebook/src/main/java/com/marin/phonebook/ui/controller/PhonebookController.java
+++ Phonebook/src/main/java/com/marin/phonebook/ui/controller/PhonebookController.java
@@ -1,5 +1,7 @@
 package com.marin.phonebook.ui.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("phonebook") // http://localhost:8080/phonebook
 public class PhonebookController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/status")
     public String getStatus() {
-        return "Phonebook Microservice is working.";
+        return "Phonebook Microservice is working on the port." + environment.getProperty("local.server.port");
     }
 
 }
